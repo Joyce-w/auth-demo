680eaf83d6cef5bb6fd9738ff8fe0da9f81ee674
Joyce <hellojoycewong@gmail.com>
2021-03-03 23:04:10 -0800

    auth demo

---
+++ app.py
@@ -0,0 +1,95 @@
+from flask import Flask, render_template, redirect, session, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, db, User, Tweet
+from forms import UserForm, TweetForm
+from sqlalchemy.exc import IntegrityError
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///auth_demo"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "abc123"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+
+connect_db(app)
+
+toolbar = DebugToolbarExtension(app)
+
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+
+@app.route('/tweets', methods=['GET', 'POST'])
+def show_tweets():
+    if "user_id" not in session:
+        flash("Please login first!", "danger")
+        return redirect('/')
+    form = TweetForm()
+    all_tweets = Tweet.query.all()
+    if form.validate_on_submit():
+        text = form.text.data
+        new_tweet = Tweet(text=text, user_id=session['user_id'])
+        db.session.add(new_tweet)
+        db.session.commit()
+        flash('Tweet Created!', 'success')
+        return redirect('/tweets')
+
+    return render_template("tweets.html", form=form, tweets=all_tweets)
+
+
+@app.route('/tweets/<int:id>', methods=["POST"])
+def delete_tweet(id):
+    """Delete tweet"""
+    if 'user_id' not in session:
+        flash("Please login first!", "danger")
+        return redirect('/login')
+    tweet = Tweet.query.get_or_404(id)
+    if tweet.user_id == session['user_id']:
+        db.session.delete(tweet)
+        db.session.commit()
+        flash("Tweet deleted!", "info")
+        return redirect('/tweets')
+    flash("You don't have permission to do that!", "danger")
+    return redirect('/tweets')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
+    form = UserForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.register(username, password)
+
+        db.session.commit()
+        return redirect('/tweets')
+
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_user():
+    form = UserForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.authenticate(username, password)
+        if user:
+            # Session something here
+            session['curr_user'] = user.id
+            return redirect('/tweets')
+
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout_user():
+    session.pop('user_id')
+    flash("Goodbye!", "info")
+    return redirect('/')
